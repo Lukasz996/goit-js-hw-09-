87c8b429273f314a8b69f35d47ef2c2b86d381d4
Łukasz <lukaszkogut1996@gmail.com>
2023-11-30 20:36:20 +0100

    fixed task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,17 +1,27 @@
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 let timer = null;
+let isRunning = false;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 startBtn.addEventListener('click', () => {
-  timer = setInterval(() => {
-    const body = document.querySelector('body');
-    body.style.background = getRandomHexColor();
-  }, 1000);
+  if (!isRunning) {
+    isRunning = true;
+    startBtn.disabled = true;
+    timer = setInterval(() => {
+      const body = document.querySelector('body');
+      body.style.background = getRandomHexColor();
+    }, 1000);
+  }
 });
+
 stopBtn.addEventListener('click', () => {
-  clearInterval(timer);
+  if (isRunning) {
+    isRunning = false;
+    startBtn.disabled = false;
+    clearInterval(timer);
+  }
 });
